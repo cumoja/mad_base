f46bc696edd0c43e71e7ffcfb08343facb0cb590
cumoja <cumoja1@gsu.edu>
2022-06-15 17:18:30 -0400

    Register and Email
    Adding very basic registration

--- create_social/lib/main.dart
+++ create_social/lib/main.dart
@@ -1,50 +1,25 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:create_social/pages/authentication.dart';
 
-void main() {
+import 'widgets/loading.dart';
+
+Future<void> main() async {
   //Always needed for firebase
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(SocialApp());
+  await Firebase.initializeApp();
+  runApp(const SocialApp());
 }
 
 class SocialApp extends StatelessWidget {
-  SocialApp({Key? key}) : super(key: key);
-  //Usually needed
-  final Future<FirebaseApp> _initFirebase = Firebase.initializeApp();
-
+  const SocialApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SocialApp',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        body: FutureBuilder(
-          initialData: _initFirebase,
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.hasError) {
-              return const Center(
-                child: Text("Ooops and Error is here."),
-              );
-            } else if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Loading();
-            }
-            return const Center(
-              child: Text("Good to go."),
-            );
-          },
+        title: 'SocialApp',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-      ),
-    );
-  }
-}
-
-class Loading extends StatelessWidget {
-  const Loading({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(child: CircularProgressIndicator());
+        home: Authentication());
   }
 }
