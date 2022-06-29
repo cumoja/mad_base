57c5489be55f66a2101a5203959669b4c09aa410
cumoja <cumoja1@gsu.edu>
2022-06-29 16:44:33 -0400

    Updated Post Model

--- create_social/lib/models/post.dart
+++ create_social/lib/models/post.dart
@@ -3,12 +3,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Post {
   final String id;
   final String content; // The typed message or the url of the image
+  int type; // The type of message this is 0:Post 1:UploadedImage 2:Sticker
   final Timestamp createdAt; // Timestamp of message
   final String creator; // User id of the creator
 
   Post(
       {required this.id,
       required this.content,
+      required this.type,
       required this.createdAt,
       required this.creator});
 
@@ -16,11 +18,17 @@ class Post {
     return Post(
         id: id,
         content: data["content"],
+        type: data["type"] ?? 0,
         createdAt: data["createdAt"],
         creator: data["creator"]);
   }
 
   Map<String, dynamic> toJSON() {
-    return {"content": content, "createdAt": createdAt, "creator": creator};
+    return {
+      "content": content,
+      "type": type,
+      "createdAt": createdAt,
+      "creator": creator
+    };
   }
 }
